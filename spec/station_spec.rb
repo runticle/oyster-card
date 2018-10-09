3cc0a920c06066e3c609327c7c2193abd7ad20c2
Muna <mmhussein125@gmail.com>
2018-10-09 16:30:04 +0100

    Created Station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,13 @@
+require 'station'
+
+describe Station do
+  station = Station.new("Aldgate", "1")
+
+  it 'returns name of station' do
+    expect(station.name).to eq "Aldgate"
+  end
+
+  it 'returns zone of station' do
+    expect(station.zone).to eq "1"
+  end
+end
