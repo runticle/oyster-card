8900844989f56c1eb36db274069e08720086a4ae
chloeverity <chloeverity@hotmail.co.uk>
2018-10-10 17:15:14 +0100

    adding doubles to journey spec

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,25 +3,35 @@ require 'station'
 require 'oystercard'
 
 describe Journey do
+
+  let (:station) { double :station }
+  let (:station2) { double :station }
+  subject {described_class.new(station, station2)}
+
+  it 'can store entry station' do
+    expect(subject.entry).to eq station
+  end
+
+  it 'can store exit station' do
+    expect(subject.exit).to eq station2
+  end
+
   it 'stores entry and exit station' do
     card = Oystercard.new
     card.topup(10)
-    station1 = Station.new("borough", '1')
-    card.touch_in(station1)
-    card.touch_out(station1)
-    expect(card.journeys).to include ({ entry: station1, exit: station1 })
+    card.touch_in(station)
+    card.touch_out(station2)
+    expect(card.journeys).to include ({ entry: station, exit: station2 })
   end
-  
+
   it 'stores multiple entries and exits' do
     card = Oystercard.new
     card.topup(10)
-    station1 = Station.new("borough", '1')
-    card.touch_in(station1)
-    card.touch_out(station1)
-    station2 = Station.new("aldgate", '1')
+    card.touch_in(station)
+    card.touch_out(station)
     card.touch_in(station2)
     card.touch_out(station2)
-    expect(card.journeys).to eq([ { entry: station1, exit: station1 },
+    expect(card.journeys).to eq([ { entry: station, exit: station },
                                   { entry: station2, exit: station2 } ])
   end
 end
